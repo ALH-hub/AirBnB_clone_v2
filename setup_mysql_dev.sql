623ab208e85fb22e979709e46441ffc7303f9a8b
Alhadji Oumate <alhadjioum0@gmail.com>
2024-01-17 11:52:54 +0100

    setup the database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,7 @@
+--Create a mysql server for the project
+    --the DATABASE hbnb_dev_db -- Create a new database called 'hbnb_dev_db'
+    -- Connect to the 'master' database to run this snippet
+    
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
     IF NOT EXISTS 'hbnb_dev'@'localhost'
