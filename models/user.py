89fc887780fc145d2704c87323f64e5748ccbe45
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-01-20 23:43:59 +0000

    update user

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """the user class"""
 from models.base_model import BaseModel
-
+from sqlalchemy import  Column, String
 
 class User(BaseModel):
     """user class definition
@@ -13,7 +13,16 @@ class User(BaseModel):
         last_name (str): user's last name
     """
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('place', backref='user', cascade='all, delete-orphan')
+        reviews = relationship('Review', backref='user', cascade='all, delete-orphan')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
