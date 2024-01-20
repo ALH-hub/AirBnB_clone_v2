b54450138877f2bf464ed5e6a8ceb9d1ee65d558
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-01-20 22:33:30 +0000

    ciites

--- models/city.py
+++ models/city.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """the city class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import COlumn, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
@@ -10,6 +13,11 @@ class City(BaseModel):
         state_id (str): state id
         name (str): city name
     """
-
+    __tablename__ = 'cities':
+    if storage_type == 'db':
+        name = Column(String(128), nullbale=False)
+	stateid = Column(String(60), ForeignKey('states.id'), nullable=False)
+	places = relationship('place', backref='cities',
+                              cascade='all, delete, delete-orphan')
     state_id = ""
     name = ""
