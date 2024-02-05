23bfee54d10e7a712666f5500da0131c949716ee
Alhadji Oumate <alhadjioum0@gmail.com>
2024-02-05 02:07:12 +0100

    fabric file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# fabfile generate .tgz file from web_static file
+
+import os.path
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """create the .tgz file"""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
